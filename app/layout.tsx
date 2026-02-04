9d3fa3f71ec59b7bbf088db0b9ae8681aaf3b4cc
SystemInfomation <forsyth@ahscampus.com>
2026-02-04 21:57:13 +0000

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,6 +30,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="dark">
+      <head>
+        <script dangerouslySetInnerHTML={{ __html: `!function(){window.semaphore=window.semaphore||[],window.ketch=function(){window.semaphore.push(arguments)};var e=new URLSearchParams(document.location.search),n=document.createElement("script");n.type="text/javascript", n.src="https://global.ketchcdn.com/web/v3/config/forsyth_county_schools/website_smart_tag/boot.js", n.defer=n.async=!0,document.getElementsByTagName("head")[0].appendChild(n)}();` }} />
+      </head>
       <body className={`${inter.className} min-h-screen`}>
         <div className="fixed inset-0 bg-gradient-cosmic -z-10" />
         <Protection />
